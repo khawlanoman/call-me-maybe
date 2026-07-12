c6d96953fe46fdb38d9a3934260c960c3d826fd2
noma <125211901+khawlanoman@users.noreply.github.com>
2026-07-12 13:52:57 +0000

    .

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from transformers import AutoTokenizer
 tokenizer = AutoTokenizer.from_pretrained("GPT2")
 text = "Hello khawla"
 
-model = Small_LLM_Model()
+#model = Small_LLM_Model()
 print(tokenizer.tokenize(text))
-print(model.encode(text))
+#print(model.encode(text))
+#print(tokenizer(text)) #DICT
+print(tokenizer(text, return_tensors="pt")) # dict with value pytorch tensors
