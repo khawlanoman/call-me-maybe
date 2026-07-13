d7012a9368aa81f3677ddf785add763af3ceac86
noma <125211901+khawlanoman@users.noreply.github.com>
2026-07-13 09:14:48 +0000

    .

--- src/start.py
+++ src/start.py
@@ -1,3 +1,10 @@
+from llm_sdk.llm_sdk import Small_LLM_Model
+
+
+def convert_to_token_ids(full_prompt: str) -> None:
+
+    model = Small_LLM_Model()
+    print(model.encode(full_prompt))
 
 def llm_prompt(prompts: list, functions: list) -> None:
     
@@ -10,7 +17,7 @@ def llm_prompt(prompts: list, functions: list) -> None:
             for param_name, param in function.parameters.items():
                 full_prompt += f"{param_name} = {param.type} "
                 # print(f"- {param_name}: {param.type}")
+        convert_to_token_ids(full_prompt)
         break        
 
-    print(full_prompt)
-
+    #print(full_prompt)
